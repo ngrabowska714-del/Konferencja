7a0eb8258cc06be8f8e1c8b8974c1708c5d6a374
nataljax <ngrabowska714@gmail.com>
2026-06-04 11:09:41 +0200

    Refactor main function to manage presentations

--- ProjektC/main.c
+++ ProjektC/main.c
@@ -1,37 +1,43 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include "konferencja.h"
 #include "pliki.h"
 
+#define MAX_PRELEKCJI 100
+
 int main() {
     int wybor;
+    Prelekcja wszystkiePrelekcje[MAX_PRELEKCJI];
+    int aktualnaLiczba = 0;
+
+    const char* nazwaPliku = "prelekcje.txt";
 
     do {
-        // Wyœwietlanie opcji
-        printf("\nPROJEKT KONFERENCJA\n");
+        printf("\nKONFERENCJA\n");
+        printf("Aktualna liczba prelekcji w pamieci: %d\n", aktualnaLiczba);
         printf("1. Dodaj prelekcje recznie\n");
         printf("2. Wczytaj prelekcje z pliku\n");
         printf("3. Oblicz wymagana liczbe sal\n");
         printf("4. Zapisz prelekcje do pliku\n");
         printf("5. Wyjdz z programu\n");
         printf("Wybierz opcje: ");
+
         scanf("%d", &wybor);
 
         switch (wybor) {
         case 1:
-            printf("\n Opcja: Dodawanie prelekcji \n");
-            // dodajPrelekcje(); <- funkcja Igora
+            printf("\nOpcja: Dodawanie prelekcji\n");
             break;
         case 2:
-            printf("\n Opcja: Wczytywanie z pliku \n");
+            printf("\nOpcja: Wczytywanie z pliku\n");
+            wczytajZPliku(nazwaPliku, wszystkiePrelekcje, &aktualnaLiczba, MAX_PRELEKCJI);
             break;
         case 3:
-            printf("\n Opcja: Obliczanie sal \n");
-            // obliczSale(); <- funkcja Igora
+            printf("\nOpcja: Obliczanie sal\n");
             break;
         case 4:
-            printf("\n Opcja: Zapis do pliku \n");
+            printf("\nOpcja: Zapis do pliku\n");
+            zapiszDoPliku(nazwaPliku, wszystkiePrelekcje, aktualnaLiczba);
             break;
         case 5:
             printf("\nZamykanie programu...\n");
